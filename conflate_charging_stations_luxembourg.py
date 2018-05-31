c3ba631cf569d7ea7171b03c00502d7e90b97367
David Morais Ferreira <david.moraisferreira@gmail.com>
2018-05-31 19:55:19 +0200

    Improved conflation profile, added requirements.txt

--- conflate_charging_stations_luxembourg.py
+++ conflate_charging_stations_luxembourg.py
@@ -1,13 +1,18 @@
+import conflate
 import json
 import io
 
+#Documentation: https://wiki.openstreetmap.org/wiki/OSM_Conflator
+
 source = "https://data.public.lu/fr/datasets/bornes-de-chargement-publiques-pour-voitures-electriques/"
-query = [('amenity', 'charging_station')]
+wiki = "https://wiki.openstreetmap.org/wiki/Import/Catalogue/Chargy_Import_Luxembourg"
+query = [('amenity', 'charging_station'), ('ref:chargy',)]
 bbox = True
 no_dataset_id = True
-master_tags = ['name','capacity','devices','socket:type2']
+max_distance = 200
+master_tags = ['name', 'capacity', 'devices', 'socket:type2', 'socket:type2:output', 'ref:chargy']
 remove_duplicates = False
-
+delete_unmatched = True
 
 def dataset(fileobj):
     js = json.loads(fileobj.read().decode('utf-8'))
